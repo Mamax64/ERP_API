d609a2cfacefb32d5b236d31adfceb22fe75c05c
Mamax64 <maxence.cuguen@viacesi.fr>
2020-01-22 17:21:33 +0100

    initial commit

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ERP_Api.Models;
+
+namespace ERP_Api.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private ERP_WEB_CLIENTEntities db = new ERP_WEB_CLIENTEntities();
+
+        // GET: api/Clients
+        public IQueryable<Client> GetClients()
+        {
+            return db.Clients;
+        }
+
+        // GET: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> GetClient(int id)
+        {
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(client);
+        }
+
+        // PUT: api/Clients/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutClient(int id, Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != client.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clients
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clients.Add(client);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [ResponseType(typeof(Client))]
+        public async Task<IHttpActionResult> DeleteClient(int id)
+        {
+            Client client = await db.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            db.Clients.Remove(client);
+            await db.SaveChangesAsync();
+
+            return Ok(client);
+        }
+
+        [Route("api/RemoveClients/{idClient}")]
+        public async Task<IHttpActionResult> RemoveClientsInfo(int idClient)
+        {
+ 
+            db.Clients.Find(idClient).Telephone = "Donnees Supprimees";
+            db.Clients.Find(idClient).Email = "Donnees Supprimees";
+            db.Clients.Find(idClient).Nom = "Donnees Supprimees";
+
+            await db.SaveChangesAsync();
+
+            return Ok(db.Clients.Find(idClient));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return db.Clients.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Controllers/ContratsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ERP_Api.Models;
+
+namespace ERP_Api.Controllers
+{
+    public class ContratsController : ApiController
+    {
+        private ERP_WEB_CLIENTEntities db = new ERP_WEB_CLIENTEntities();
+
+        // GET: api/Contrats
+        public IQueryable<Contrat> GetContrats()
+        {
+            return db.Contrats;
+        }
+
+        // GET: api/Contrats/5
+        [ResponseType(typeof(Contrat))]
+        public async Task<IHttpActionResult> GetContrat(int id)
+        {
+            Contrat contrat = await db.Contrats.FindAsync(id);
+            if (contrat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contrat);
+        }
+
+        // PUT: api/Contrats/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutContrat(int id, Contrat contrat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contrat.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contrat).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContratExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Contrats
+        [ResponseType(typeof(Contrat))]
+        public async Task<IHttpActionResult> PostContrat(Contrat contrat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contrats.Add(contrat);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = contrat.Id }, contrat);
+        }
+
+        // DELETE: api/Contrats/5
+        [ResponseType(typeof(Contrat))]
+        public async Task<IHttpActionResult> DeleteContrat(int id)
+        {
+            Contrat contrat = await db.Contrats.FindAsync(id);
+            if (contrat == null)
+            {
+                return NotFound();
+            }
+
+            db.Contrats.Remove(contrat);
+            await db.SaveChangesAsync();
+
+            return Ok(contrat);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContratExists(int id)
+        {
+            return db.Contrats.Count(e => e.Id == id) > 0;
+        }
+    }
+}
